f4ab488c7ae955393ded33ac9498e54cc0b67173
seanyexx <sean.yex@gmail.com>
2018-08-03 13:19:03 +1000

    change install

--- setup/check_library.R
+++ setup/check_library.R
@@ -1,21 +1,22 @@
 installed_packages<-installed.packages()[, "Package"]
 need_packages<-c("devtools","flowCore","FlowSOM","flowPeaks")
-if(!require(devtools)){  
-	install.packages("devtools")
-}
 for (i in need_packages){
 	if (i %in% installed_packages){
 		print(paste0(i, " is installed."))
 	}else{
-		print(paste0(i, " is not installed."))
-		library(devtools)
-		source("https://bioconductor.org/biocLite.R")
-		if(i=="flowCore"){
-			install_github("RGLab/flowCore",ref='trunk')
-		}else{
-			biocLite(i)
+		if (i=="devtools"){
+			install.packages('devtools', repos='http://cran.us.r-project.org')
 		}
-		
+		else{
+			library(devtools)
+			print(paste0(i, " is not installed."))
+			source("https://bioconductor.org/biocLite.R")
+			if(i=="flowCore"){
+				install_github("RGLab/flowCore",ref='trunk')
+			}else{
+				biocLite(i)
+			}
+		}
 	}
 }
 
