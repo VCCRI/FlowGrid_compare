c65b0a5d1631f6d3fcf2adc9f07ee32f69b9b442
seanyexx <sean.yex@gmail.com>
2018-08-03 13:34:30 +1000

    change

--- code/run_all.sh
+++ code/run_all.sh
@@ -1,9 +1,9 @@
-'''
-The following code is to install the required packages.
+
+#The following code is to install the required packages.
 
 pip install -r ../setup/requirement.txt  --user
 Rscript ../setup/check_library.R
-'''
+
 ## downloading the seaflow data and preprocessing them.
 sh down.sh
 
