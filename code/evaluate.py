8546715eff623745f19c1c67eeaf2f9140de6751
seanyexx <sean.yex@gmail.com>
2018-08-05 17:33:33 +1000

    update

--- code/evaluate.py
+++ code/evaluate.py
@@ -1,26 +1,16 @@
 from sklearn.metrics import adjusted_rand_score as ARI
-from sklearn.metrics import confusion_matrix as cm
 import numpy as np
+import pandas as pd
 import sys
-import matplotlib
-matplotlib.use("agg")
-import matplotlib.pyplot as plt
-import seaborn as sns
-
+import os
 loc_d,data,alg,loc_l=[sys.argv[i] for i in range(1,5)]
 pred_file="../"+loc_d+"/"+data+"_"+alg+"_predict.csv"
 label_file="../"+loc_l+"/"+data+"_label.csv"
 if alg!="Flock":
-	label=np.genfromtxt(pred_file, delimiter='\t', skip_header=1)
-	
+	label=pd.read_csv(pred_file,sep=",")
 else:
-	label=np.genfromtxt(pred_file, delimiter='\t')
-true_label=np.genfromtxt(label_file, delimiter='\t')
+	label=pd.read_csv(pred_file,sep=",",header=None)
+true_label=pd.read_csv(label_file,sep=",",header=None)
 true_label=np.array(true_label).reshape(-1)
 label=np.array(label).reshape(-1)
 print(alg+",ARI,"+data+","+str(round(ARI(label,true_label),4)))
-
-'''
-cma=cm(label,true_label)
-sns.heatmap(cma, annot=True)
-plt.savefig(data+"_"+alg+".png")'''
--- code/transform.py
+++ code/transform.py
@@ -5,7 +5,7 @@ file=sys.argv[1]
 df=pd.read_csv(file,sep=',')
 data_df=df[["fsc_small","fsc_perp","pe","chl_small"]]
 label_df=df["pop"]
-tran_df=np.arcsinh(data_df/5)
+tran_df=np.arcsinh((data_df-1)/5)
 tran_df=tran_df.round(4)
 tran_df.to_csv("../data/"+file[:-4]+"_data.csv",sep="\t",index=False)
 label_df.to_csv("../label/"+file[:-4]+"_label.csv",sep="\t",index=False)
